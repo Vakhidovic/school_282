13362119eed716332bc1d02ec10dcb918548ac1d
wayne <bahodirrisqulov176@gmail.com>
2022-05-19 15:24:06 +0500

    3

--- main_codes don't touch it/282 school project/js/mainScript.js
+++ main_codes don't touch it/282 school project/js/mainScript.js
@@ -10,3 +10,31 @@ menuBtn.addEventListener('click', ()=> {
         menuOpen=false;
     }
 })
+
+
+
+
+
+
+
+
+// function my4(id) {
+//     switch (id) {
+//         case 1:
+//             document.querySelector("#x").innerHTML = `
+//                    <button onclick="du()">A</button>
+//                    <button onclick="du()">B</button>
+//                    <button onclick="du()">V</button>
+//                    <button onclick="du()">E</button>
+//                    <button onclick="du()">G</button>`;
+//             break;
+//         default:
+//             document.querySelector("#x").innerHTML = "Bunday sinf yoq";
+//             break;
+//     }
+// }
+//
+// function a9() {
+//     var a = 1;
+//     my4(a);
+// }
