2b8d06a45b8a6d5de99a1fb64708f618305f18da
wayne <104674638+umarofWayne@users.noreply.github.com>
2022-05-22 15:28:16 +0300

    dd

--- main_codes don't touch it/282 school project/js/mainScript.js
+++ main_codes don't touch it/282 school project/js/mainScript.js
@@ -165,10 +165,17 @@ menuBtn.addEventListener('click', ()=> {
 
 
 
-
-
-
-
+var header = document.querySelector(".buttons");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        let current = document.getElementsByClassName("active");
+        if (current.length > 0) {
+            current[0].className = current[0].className.replace(" active", "");
+        }
+        this.className += " active";
+    });
+}
 
 
 
