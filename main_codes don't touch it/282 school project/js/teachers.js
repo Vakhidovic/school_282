6ee14038dd881d09b93524579ac60a6bd821f322
wayne <bahodirrisqulov176@gmail.com>
2022-05-26 15:16:32 +0500

    gh

---
+++ main_codes don't touch it/282 school project/js/teachers.js
@@ -0,0 +1,10 @@
+// function teachers1() {
+//     document.querySelector(".text__teachers").innerHTML = `
+//     <h4>Сергей Буйняков4 </h4>
+//  `
+// }
+//
+// function teachers2() {
+//     document.querySelector(".text__teachers").innerHTML= `
+//      <h4>Сергей Буйняков2</h4>`
+// }
